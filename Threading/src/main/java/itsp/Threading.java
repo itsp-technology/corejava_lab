741ac4f6ae82bdc8d35a710f9e4776d9a4d5b389
itsp-technology <vivek1996.cloud@gmail.com>
2025-03-05 13:35:23 +0530

    up

--- Threading/src/main/java/itsp/Threading.java
+++ Threading/src/main/java/itsp/Threading.java
@@ -2,6 +2,6 @@ package itsp;
 
 public class Threading {
     public static void main(String[] args) {
-        System.out.println("sytem vivek");
+        System.out.println("sytem vivek next");
     }
 }
